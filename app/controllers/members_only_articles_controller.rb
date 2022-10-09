161582351712a30adfa1562d525ddec325707d55
Tonny Oseko <tonnyoseko0@gmail.com>
2022-10-09 04:51:43 +0300

    commit tree 13458866dd2971bc4a6d841e259ec5c30b3e924a

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -2,11 +2,13 @@ class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     article = Article.find(params[:id])
     render json: article
   end
